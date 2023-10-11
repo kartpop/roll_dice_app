8d2e9f115d6aa86b8c6590eb81c8c7de4957fab1
KartikeyaPophali <kartikeyapophali@gmail.com>
2023-10-11 22:14:11 +0530

    feat: include image and experiment with multiple constructor functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
+
+  GradientContainer.purple({super.key})
+      : colors = [Colors.purple, Colors.indigo];
 
   final List<Color> colors;
 
@@ -19,8 +21,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
       ),
     );
   }
