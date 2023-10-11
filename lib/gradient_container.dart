449f4b4351623c783979e331bab6edde890b82d0
KartikeyaPophali <kartikeyapophali@gmail.com>
2023-10-11 12:03:21 +0530

    chore: refactor gradient container in separate class

---
+++ lib/gradient_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(233, 27, 31, 82),
+            Color.fromARGB(208, 8, 15, 112)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,11 @@
 import "package:flutter/material.dart";
+import "package:roll_dice_app/gradient_container.dart";
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(233, 27, 31, 82),
-                Color.fromARGB(208, 8, 15, 112)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
